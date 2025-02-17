e283a4cbe5c7504ff078fe9f88393dec185ff37e
Stan Angeloff <stanimir@angeloff.name>
2025-02-17 14:37:34 +0200

    fix: Use Wayland for Viber.

--- home/apps/viber/default.nix
+++ home/apps/viber/default.nix
@@ -3,7 +3,7 @@
   package = pkgs.viber.overrideAttrs (
     finalAttrs: previousAttrs: {
       installPhase = ''
-        ${previousAttrs.installPhase or ""}
+        ${builtins.replaceStrings [ "\"xcb\"" ] [ "\"wayland\"" ] (previousAttrs.installPhase or "")}
 
         substituteInPlace $out/share/applications/viber.desktop \
           --replace Path=/opt/viber/ Path=$out/opt/viber/
