0af70d2efdcdf23fbb7126b6e911394f9a6b06c2
Stan Angeloff <stanimir@angeloff.name>
2025-02-20 10:16:33 +0200

    feat: disable wake up from any device.

--- system/components/udev.nix
+++ system/components/udev.nix
@@ -1,5 +1,12 @@
 { pkgs, ... }:
 {
-  # This is a documented requirement for appindicator  ¯\_(ツ)_/¯
-  services.udev.packages = [ pkgs.gnome-settings-daemon ];
+  services.udev = {
+    # This is a documented requirement for appindicator  ¯\_(ツ)_/¯
+    packages = with pkgs; [ gnome-settings-daemon ];
+
+    # See https://nixos.wiki/wiki/Power_Management#Solution_1:_Disabling_wakeup_triggers_for_all_PCIe_devices
+    extraRules = ''
+      ACTION=="add", SUBSYSTEM=="pci", DRIVER=="pcieport", ATTR{power/wakeup}="disabled"
+    '';
+  };
 }
