5737f315115292e1038c60bec6f4ad4098e4da9f
Stan Angeloff <stanimir@angeloff.name>
2025-02-21 14:20:45 +0200

    feat: update claudius.nvim

--- home/apps/nixvim/plugins/claudius-nvim.nix
+++ home/apps/nixvim/plugins/claudius-nvim.nix
@@ -9,8 +9,8 @@
             src = pkgs.fetchFromGitHub {
               owner = "StanAngeloff";
               repo = "claudius.nvim";
-              rev = "8eaf30422251c6b61c52d8a333504e60ab461c4b";
-              hash = "sha256-1TwJz7T10OVpraHgN2nRBdrtI1hSzBmVHUNdBEgQ2Qg=";
+              rev = "4050291456fcba371930bfa0bf9f53c929a765bd";
+              hash = "sha256-sC92vwmvgx+6B7movZFTo4ez5sUy3UmGbWGAmOLYCx4=";
             };
           }
         );
