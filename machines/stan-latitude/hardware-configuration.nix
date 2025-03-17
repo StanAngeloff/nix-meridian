98eca747d203fb70aa37fc6adffa9cd04bacccf5
Stan Angeloff <stanimir@angeloff.name>
2025-03-17 11:54:09 +0200

    feat: add printing support for Samsung M2022W.

--- machines/stan-latitude/hardware-configuration.nix
+++ machines/stan-latitude/hardware-configuration.nix
@@ -66,4 +66,11 @@
   hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
 
   services.fprintd.tod.driver = pkgs.libfprint-2-tod1-broadcom;
+
+  services.printing = {
+    drivers = with pkgs; [
+      # Samsung M2022W
+      samsung-unified-linux-driver
+    ];
+  };
 }
--- system/components/printing.nix
+++ system/components/printing.nix
@@ -1,4 +1,15 @@
 {
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  #
+  # Learn more at https://nixos.wiki/wiki/Printing#Installation
+  services.printing = {
+    enable = true;
+  };
+
+  # Learn more at https://nixos.wiki/wiki/Printing#Enable_autodiscovery_of_network_printers
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
