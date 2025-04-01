30a3653af8e3ff3201c3faf49960877abd1c4a8a
Stan Angeloff <stanimir@angeloff.name>
2025-04-01 16:25:35 +0300

    feat: *ProtonPass 1.29.{2 ⇒ 8}

--- home/apps/proton-pass/default.nix
+++ home/apps/proton-pass/default.nix
@@ -3,9 +3,9 @@
   fetchurl,
 }:
 proton-pass.overrideAttrs (finalAttrs: rec {
-  version = "1.29.2"; # Override the version, as of 2025-02-11 the NixOS version is outdated.
+  version = "1.29.8"; # Override the version, as of 2025-02-11 the NixOS version is outdated.
   src = fetchurl {
     url = "https://proton.me/download/pass/linux/x64/proton-pass_${version}_amd64.deb";
-    hash = "sha256-swq4zDcBephqqtabg36eDSWubo0LDpScUxB30RnzGHQ=";
+    hash = "sha256-xwXIcoBPhANKiTv7DP+gmVeCtuzjymtQmHu+HuRKqjU=";
   };
 })
