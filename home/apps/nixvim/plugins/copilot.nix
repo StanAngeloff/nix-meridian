377c84e8e60b3fa3ded1440a21389bca571b3bb6
Stan Angeloff <stanimir@angeloff.name>
2025-04-09 10:46:41 +0300

    feat: use copilot.lua from nixpkgs-unstable.

--- home/apps/nixvim/plugins/copilot.nix
+++ home/apps/nixvim/plugins/copilot.nix
@@ -1,7 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   programs.nixvim.plugins.copilot-lua = {
     enable = true;
+    package = pkgs-unstable.vimPlugins.copilot-lua;
 
     copilotNodeCommand = "${pkgs.nodejs_22}/bin/node";
 
