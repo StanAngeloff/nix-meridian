1521c00b235131677c78b9cac3613ab0517a6aab
Stan Angeloff <stanimir@angeloff.name>
2025-04-16 12:13:56 +0300

    feat: update claudius.nvim, pass settings as Nix object.

--- home/apps/nixvim/plugins/claudius-nvim.nix
+++ home/apps/nixvim/plugins/claudius-nvim.nix
@@ -1,4 +1,34 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+let
+  nixvim = config.lib.nixvim;
+  claudius-settings = {
+    parameters = {
+      max_tokens = 8000;
+    };
+    editing = {
+      auto_write = true;
+    };
+    pricing = {
+      enabled = true;
+    };
+    ruler = {
+      char = "━";
+    };
+    signs = {
+      enabled = true;
+      assistant = {
+        hl = "#8f9fdf";
+      };
+      user = {
+        char = "▏";
+        hl = "#6f6f6f";
+      };
+    };
+    highlights = {
+      assistant = "#8f9faf";
+    };
+  };
+in
 {
   programs.nixvim = {
     extraPlugins =
@@ -9,8 +39,8 @@
             src = pkgs.fetchFromGitHub {
               owner = "StanAngeloff";
               repo = "claudius.nvim";
-              rev = "cac7d0f6a72474ef1c8fec4a27e4982f353caaf8";
-              hash = "sha256-obSWGtYPJahqESCv3s/qmsPV/bijNbfv53pCML0qMj8=";
+              rev = "9d99e727de40ebe169535065c987aed459a7620c";
+              hash = "sha256-22W/7X2406frxb8/p8VLuUA6XqzifrZtdbfY/4B1tfA=";
             };
           }
         );
@@ -20,20 +50,7 @@
       ];
 
     extraConfigLua = ''
-      require("claudius").setup({
-        parameters = {
-          max_tokens = 8000,
-        },
-        editing = {
-          auto_write = true,
-        },
-        pricing = {
-          enabled = true,
-        },
-        signs = {
-          enabled = true,
-        },
-      })
+      require("claudius").setup(${nixvim.toLuaObject claudius-settings});
     '';
   };
 }
