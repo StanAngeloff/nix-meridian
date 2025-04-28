b26ddb1daca97c8fd363558d6066fd6444c82f9c
Stan Angeloff <stanimir@angeloff.name>
2025-04-28 12:52:47 +0300

    feat: use unstable nvim-autopairs.

--- home/apps/nixvim/plugins/nvim-autopairs.nix
+++ home/apps/nixvim/plugins/nvim-autopairs.nix
@@ -1,6 +1,8 @@
+{ pkgs-unstable, ... }:
 {
   programs.nixvim.plugins.nvim-autopairs = {
     enable = true;
+    package = pkgs-unstable.vimPlugins.nvim-autopairs;
 
     settings = {
       check_ts = true;
