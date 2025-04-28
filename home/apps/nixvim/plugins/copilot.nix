f137c5765400115aa3c6fdeb90a4a77a0a7c889b
Stan Angeloff <stanimir@angeloff.name>
2025-04-28 12:52:24 +0300

    feat: override "Copilot*" highlights in Neovim.

--- home/apps/nixvim/plugins/copilot.nix
+++ home/apps/nixvim/plugins/copilot.nix
@@ -1,18 +1,25 @@
 { pkgs, pkgs-unstable, ... }:
 {
-  programs.nixvim.plugins.copilot-lua = {
-    enable = true;
-    package = pkgs-unstable.vimPlugins.copilot-lua;
+  programs.nixvim = {
+    plugins.copilot-lua = {
+      enable = true;
+      package = pkgs-unstable.vimPlugins.copilot-lua;
 
-    copilotNodeCommand = "${pkgs.nodejs_22}/bin/node";
+      copilotNodeCommand = "${pkgs.nodejs_22}/bin/node";
 
-    suggestion = {
-      enabled = true;
-      autoTrigger = true;
+      suggestion = {
+        enabled = true;
+        autoTrigger = true;
+      };
+
+      filetypes = {
+        markdown = true;
+      };
     };
 
-    filetypes = {
-      markdown = true;
-    };
+    extraConfigLua = ''
+      vim.api.nvim_set_hl(0, "CopilotSuggestion", { fg = "#00a3a2", bg = "#003131", italic = true })
+      vim.api.nvim_set_hl(0, "CopilotAnnotation", { fg = "#00a3a2", bg = "#003131", bold = true })
+    '';
   };
 }
