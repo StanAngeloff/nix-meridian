fea9d0b8e82d4aa9e8f2a0dbc9bc21d2481d7d97
Stan Angeloff <stanimir@angeloff.name>
2025-05-06 14:53:10 +0300

    feat: block loading Noto Color Emoji in Viber.

--- home/apps/viber/default.nix
+++ home/apps/viber/default.nix
@@ -2,7 +2,17 @@
 viber.overrideAttrs (
   finalAttrs: previousAttrs: {
     installPhase = ''
-      ${builtins.replaceStrings [ "\"xcb\"" ] [ "\"wayland\"" ] (previousAttrs.installPhase or "")}
+      ${builtins.replaceStrings
+        [
+          "--set QT_QPA_PLATFORM \"xcb\""
+          "--set QML2_IMPORT_PATH"
+        ]
+        [
+          "--set QT_QPA_PLATFORM \"wayland\""
+          "--set FONTCONFIG_FILE \"${./fonts.conf}\" --set QML2_IMPORT_PATH"
+        ]
+        (previousAttrs.installPhase or "")
+      }
 
       substituteInPlace $out/share/applications/viber.desktop \
         --replace Path=/opt/viber/ Path=$out/opt/viber/
