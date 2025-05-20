dbb3540eb503cff01294c48d55816b0ed958672d
Stan Angeloff <stanimir@angeloff.name>
2025-05-20 10:25:26 +0300

    feat: switch claudius.nvim to gemini-2.5-pro-preview-05-06.

--- home/apps/nixvim/plugins/claudius-nvim.nix
+++ home/apps/nixvim/plugins/claudius-nvim.nix
@@ -1,9 +1,29 @@
 { config, pkgs, ... }:
 let
   nixvim = config.lib.nixvim;
+  claudius-nvim = (
+    pkgs.vimUtils.buildVimPlugin {
+      name = "claudius.nvim";
+      src = pkgs.fetchFromGitHub {
+        owner = "StanAngeloff";
+        repo = "claudius.nvim";
+        rev = "3aba6f8dad04396e94bd9df161e0839fe87d9779";
+        hash = "sha256-3kuAo1yDw6DZ71I9ONIghs6Tu+q+cI161w13JmiKATs=";
+      };
+
+      postInstall = ''
+        substituteInPlace $target/lua/claudius/provider/vertex.lua \
+          --replace gcloud "${pkgs.google-cloud-sdk}/bin/gcloud"
+      '';
+    }
+  );
   claudius-settings = {
+    provider = "vertex";
+    model = "gemini-2.5-pro-preview-05-06";
     parameters = {
       max_tokens = 8000;
+      timeout = 300;
+      project_id = "stans-playground";
     };
     editing = {
       auto_write = true;
@@ -31,23 +51,7 @@ let
 in
 {
   programs.nixvim = {
-    extraPlugins =
-      let
-        claudius-nvim = (
-          pkgs.vimUtils.buildVimPlugin {
-            name = "claudius.nvim";
-            src = pkgs.fetchFromGitHub {
-              owner = "StanAngeloff";
-              repo = "claudius.nvim";
-              rev = "3aba6f8dad04396e94bd9df161e0839fe87d9779";
-              hash = "sha256-3kuAo1yDw6DZ71I9ONIghs6Tu+q+cI161w13JmiKATs=";
-            };
-          }
-        );
-      in
-      [
-        claudius-nvim
-      ];
+    extraPlugins = [ claudius-nvim ];
 
     extraConfigLua = ''
       require("claudius").setup(${nixvim.toLuaObject claudius-settings});
