213bb95c4e028b6a62c77dcf99092c4b38e695cf
Stan Angeloff <stanimir@angeloff.name>
2025-05-22 14:26:17 +0300

    feat: enable "idiomatic_version_file_enable_tools" in mise.

--- home/apps/mise/default.nix
+++ home/apps/mise/default.nix
@@ -2,5 +2,12 @@
   programs.mise = {
     enable = true;
     enableZshIntegration = true;
+
+    # See https://mise.jdx.dev/configuration.html#global-config-config-mise-config-toml
+    globalConfig = {
+      settings = {
+        idiomatic_version_file_enable_tools = [ "node" ];
+      };
+    };
   };
 }
