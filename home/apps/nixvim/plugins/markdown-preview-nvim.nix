f1423b81823fa5e99c38c821da262ab529c8e0c9
Stan Angeloff <stanimir@angeloff.name>
2025-06-09 12:52:11 +0300

    feat: inherit default fonts in markdown-preview.nvim

--- home/apps/nixvim/plugins/markdown-preview-nvim.nix
+++ home/apps/nixvim/plugins/markdown-preview-nvim.nix
@@ -1,30 +1,37 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
 {
   programs.nixvim = {
-    extraPlugins = with pkgs.vimPlugins; [
-      markdown-preview-nvim
-    ];
+    plugins.markdown-preview = {
+      enable = true;
+      package = pkgs-unstable.vimPlugins.markdown-preview-nvim;
 
-    globals = {
-      mkdp_browser = "${pkgs.brave}/bin/brave";
-      mkdp_theme = "light";
-      mkdp_refresh_slow = 1;
-
-      mkdp_preview_options = {
-        "mkit" = {
-          "breaks" = 1;
+      settings = {
+        browser = "${pkgs.brave}/bin/brave";
+        theme = "light";
+        refresh_slow = 1;
+        markdown_css = builtins.toString (
+          pkgs.replaceVars ../resources/markdown.css {
+            markdowncss = builtins.readFile "${pkgs-unstable.vimPlugins.markdown-preview-nvim}/app/_static/markdown.css";
+            fontsSerif = config.nix-meridian.fonts.serif.name;
+            fontsSansSerif = config.nix-meridian.fonts.sansSerif.name;
+            fontsMonospace = config.nix-meridian.fonts.monospace.name;
+          }
+        );
+        preview_options = {
+          mkit.__raw = ''
+            { breaks = 1 }
+          '';
+          content_editable = 0;
+          disable_filename = 0;
+          disable_sync_scroll = 0;
+          sync_scroll_type = "middle";
+          hide_yaml_meta = 0;
         };
-        "katex" = { };
-        "uml" = { };
-        "maid" = { };
-        "disable_sync_scroll" = 0;
-        "sync_scroll_type" = "middle";
-        "hide_yaml_meta" = 1;
-        "sequence_diagrams" = { };
-        "flowchart_diagrams" = { };
-        "content_editable" = false;
-        "disable_filename" = 0;
-        "toc" = { };
       };
     };
   };
