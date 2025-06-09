f6570e85fb9eaeaeb0ac2fab426387a7d5d27185
Stan Angeloff <stanimir@angeloff.name>
2025-06-09 12:58:18 +0300

    feat: migrate codecompanion.nvim (unused) configuration to Nixvim.

--- home/apps/nixvim/plugins/codecompanion-nvim.nix
+++ home/apps/nixvim/plugins/codecompanion-nvim.nix
@@ -1,82 +1,73 @@
-{ config, pkgs-unstable, ... }:
-let
-  nixvim = config.lib.nixvim;
-  codecompanion-settings = {
-    adapters = {
-      anthropic = {
-        __raw = ''
-          function()
-            return require("codecompanion.adapters").extend("anthropic", {
-              env = {
-                api_key = "cmd:secret-tool lookup service anthropic key api",
-              },
-            })
-          end
-        '';
-      };
-      openai = {
-        __raw = ''
-          function()
-            return require("codecompanion.adapters").extend("openai", {
-              env = {
-                api_key = "cmd:secret-tool lookup service openai key api",
-              },
-            })
-          end
-        '';
-      };
-    };
-    strategies = {
-      chat = {
-        adapter = "anthropic";
-        keymaps = {
-          close = {
-            modes = {
-              n = "q";
-              i = "<C-q>";
-            };
-          };
-          send = {
-            modes = {
-              n = "<C-]>";
-              i = "<C-]>";
-            };
-          };
-          stop = {
-            modes = {
-              n = "<C-c>";
-            };
-          };
-        };
-      };
-      inline = {
-        adapter = "copilot";
-      };
-      cmd = {
-        adapter = "anthropic";
-      };
-    };
-    display = {
-      chat = {
-        show_settings = false;
-        start_in_insert_mode = true;
-        show_header_separator = true;
-        window = {
-          layout = "float";
-        };
-      };
-    };
-  };
-in
+{ pkgs-unstable, ... }:
 {
   programs.nixvim = {
-    extraPlugins = with pkgs-unstable.vimPlugins; [
-      codecompanion-nvim
-    ];
+    plugins.codecompanion = {
+      enable = true;
+      package = pkgs-unstable.vimPlugins.codecompanion-nvim;
 
-    extraConfigLua = ''
-      require("codecompanion").setup(${nixvim.toLuaObject codecompanion-settings});
-    '';
+      settings = {
+        adapters = {
+          anthropic.__raw = ''
+            function()
+              return require("codecompanion.adapters").extend("anthropic", {
+                env = {
+                  api_key = "cmd:secret-tool lookup service anthropic key api",
+                },
+              })
+            end
+          '';
+          openai.__raw = ''
+            function()
+              return require("codecompanion.adapters").extend("openai", {
+                env = {
+                  api_key = "cmd:secret-tool lookup service openai key api",
+                },
+              })
+            end
+          '';
+        };
+        strategies = {
+          chat = {
+            adapter = "anthropic";
+            keymaps = {
+              close = {
+                modes = {
+                  n = "q";
+                  i = "<C-q>";
+                };
+              };
+              send = {
+                modes = {
+                  n = "<C-]>";
+                  i = "<C-]>";
+                };
+              };
+              stop = {
+                modes = {
+                  n = "<C-c>";
+                };
+              };
+            };
+          };
+          inline = {
+            adapter = "copilot";
+          };
+          cmd = {
+            adapter = "anthropic";
+          };
+        };
+        display = {
+          chat = {
+            show_settings = false;
+            start_in_insert_mode = true;
+            show_header_separator = true;
+            window = {
+              layout = "float";
+            };
+          };
+        };
+      };
+    };
 
     keymaps = [
       {
