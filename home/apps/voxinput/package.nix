d8621048958f42c5cd3ce1b9b8015616fb8182f3
Stan Angeloff <stanimir@angeloff.name>
2025-08-13 14:14:32 +0300

    feat: bump voxinput to v0.6.0
    The source now has all the build inputs so we can simplify the build.

--- home/apps/voxinput/package.nix
+++ home/apps/voxinput/package.nix
@@ -1,19 +1,14 @@
 {
   voxinput,
   fetchFromGitHub,
-  pkg-config,
-  libGL,
-  libxkbcommon,
-  wayland,
-  xorg,
 }:
 voxinput.overrideAttrs (previousAttrs: rec {
-  version = "b4258e60ede026d4de665999444b312a78649d34";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "richiejp";
     repo = "VoxInput";
-    rev = "${version}";
+    rev = "v${version}";
     hash = "sha256-Uxl+qr4KEBAiIMbPlBq+1vvdjRDOsKOl+JOI1hjH4TE=";
   };
 
@@ -23,36 +18,19 @@ voxinput.overrideAttrs (previousAttrs: rec {
     ./patches/0001-feat-add-support-for-injecting-a-prompt-via-env-vari.patch
   ];
 
-  nativeBuildInputs = [
-    pkg-config
-  ]
-  ++ previousAttrs.nativeBuildInputs;
-
-  buildInputs = [
-    libGL
-    libxkbcommon
-    wayland
-    xorg.libX11.dev
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXinerama
-    xorg.libXrandr
-    xorg.libXxf86vm
-  ]
-  ++ previousAttrs.buildInputs;
-
-  postInstall = ''
-    ${builtins.replaceStrings
-      [ "--prefix PATH" ]
+  postInstall =
+    let
+      marker = "--prefix PATH";
+    in
+    builtins.replaceStrings
+      [ marker ]
       [
-        (
-          "--set-default OPENAI_BASE_URL \"https://api.openai.com/v1\" "
-          + "--set-default OPENAI_WS_BASE_URL \"wss://api.openai.com/v1/realtime\" "
-          + "--run \"export OPENAI_API_KEY=\\\${OPENAI_API_KEY-\\$(secret-tool lookup service openai key api 2>/dev/null)}\" "
-          + "--prefix PATH"
-        )
+        (builtins.concatStringsSep " " [
+          "--set-default OPENAI_BASE_URL \"https://api.openai.com/v1\""
+          "--set-default OPENAI_WS_BASE_URL \"wss://api.openai.com/v1/realtime\""
+          "--run \"export OPENAI_API_KEY=\\\${OPENAI_API_KEY-\\$(secret-tool lookup service openai key api 2>/dev/null)}\""
+          marker
+        ])
       ]
-      (previousAttrs.postInstall or "")
-    }
-  '';
+      (previousAttrs.postInstall or "");
 })
