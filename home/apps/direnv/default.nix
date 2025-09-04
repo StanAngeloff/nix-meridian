a2fe660ed71f9fb8b57caa6928f8773ec7e79e63
Stan Angeloff <stanimir@angeloff.name>
2025-09-04 15:25:33 +0300

    feat: +nix-direnv

--- home/apps/direnv/default.nix
+++ home/apps/direnv/default.nix
@@ -4,6 +4,9 @@
     enable = true;
     enableZshIntegration = true;
 
+    # Learn more at https://github.com/nix-community/nix-direnv
+    nix-direnv.enable = true;
+
     mise = {
       enable = true;
       package = pkgs-unstable.mise;
