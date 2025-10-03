0f2a311f6dc5a64f657f5ca6bdfec91b0fd84119
Stan Angeloff <stanimir@angeloff.name>
2025-10-03 10:33:29 +0300

    feat: add android-udev-rules

--- system/components/udev.nix
+++ system/components/udev.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
 {
   services.udev = {
-    # This is a documented requirement for appindicator  ¯\_(ツ)_/¯
-    packages = with pkgs; [ gnome-settings-daemon ];
+    packages = with pkgs; [
+      # Learn more at https://wiki.nixos.org/wiki/GNOME#Enable_system_tray_icons ¯\_(ツ)_/¯
+      gnome-settings-daemon
+      # The below is needed to enable "Transferring images" USB configuration.
+      # Learn more at https://github.com/M0Rf30/android-udev-rules
+      android-udev-rules
+    ];
 
     # See https://nixos.wiki/wiki/Power_Management#Solution_1:_Disabling_wakeup_triggers_for_all_PCIe_devices
     extraRules = ''
