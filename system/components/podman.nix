4197ce35584971d14fa64862daac813fe93e352f
Stan Angeloff <stanimir@angeloff.name>
2025-10-10 13:23:39 +0300

    fix: escape DOCKER_HOST as systemd won't accept it otherwise.

--- system/components/podman.nix
+++ system/components/podman.nix
@@ -33,5 +33,5 @@
     docker-compose
   ];
 
-  environment.sessionVariables.DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/podman/podman.sock";
+  environment.sessionVariables.DOCKER_HOST = "unix://\${XDG_RUNTIME_DIR}/podman/podman.sock";
 }
