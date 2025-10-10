cbc3680603614b34cd6e785ab25c0b018c62fe7e
Stan Angeloff <stanimir@angeloff.name>
2025-10-10 13:37:55 +0300

    feat: refactor Android rules into adb.nix

--- system/components/adb.nix
+++ system/components/adb.nix
@@ -1,5 +1,17 @@
+{ pkgs, ... }:
 {
   programs.adb = {
+    enable = true;
+  };
+
+  services.udev.packages = with pkgs; [
+    # The below is needed to enable "Transferring images" USB configuration.
+    # Learn more at https://github.com/M0Rf30/android-udev-rules
+    android-udev-rules
+  ];
+
+  services.gvfs = {
+    # GVfs, a userspace virtual filesystem, is needed for file transfer over MTP.
     enable = true;
   };
 }
--- system/components/udev.nix
+++ system/components/udev.nix
@@ -4,9 +4,6 @@
     packages = with pkgs; [
       # Learn more at https://wiki.nixos.org/wiki/GNOME#Enable_system_tray_icons ¯\_(ツ)_/¯
       gnome-settings-daemon
-      # The below is needed to enable "Transferring images" USB configuration.
-      # Learn more at https://github.com/M0Rf30/android-udev-rules
-      android-udev-rules
     ];
 
     # See https://nixos.wiki/wiki/Power_Management#Solution_1:_Disabling_wakeup_triggers_for_all_PCIe_devices
