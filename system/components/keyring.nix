255d687f77a29be170cef481ec42ba644cd7cbec
Stan Angeloff <stanimir@angeloff.name>
2025-11-11 17:51:08 +0200

    fix(security): don't attempt to auto-unlock GNOME Keyring.
    This is currently broken on NixOS when using LUKS and auto-login.

--- system/components/keyring.nix
+++ system/components/keyring.nix
@@ -3,7 +3,9 @@
     enable = true;
   };
 
-  security.pam.services.login = {
-    enableGnomeKeyring = true;
-  };
+  # NOTE: This is currently broken on NixOS when using LUKS and GDM auto-login.
+  #       Learn more at https://github.com/NixOS/nixpkgs/pull/286587 which will introduce a fix using `pam_systemd_loadkey`.
+  #security.pam.services.gdm-password = {
+  #  enableGnomeKeyring = true;
+  #};
 }
