a11b66ad832250fe2b4fa6f01032fd51f2d47f9f
Stan Angeloff <stanimir@angeloff.name>
2025-12-23 11:33:39 +0200

    refactor: merge packages into a single file.

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,13 +1,17 @@
 {
+  lib,
   pkgs,
   pkgs-unstable,
-  voxinput-pkgs,
   ...
 }:
 let
-  packages = pkgs.callPackage ./packages.nix {
-    inherit pkgs pkgs-unstable voxinput-pkgs;
+  bruno = pkgs.callPackage ./bruno/package.nix { };
+  clipboard2markdown = pkgs.callPackage ./clipboard2markdown/package.nix {
+    inherit (pkgs-unstable) pandoc;
   };
+  nsx = pkgs.callPackage ./nsx/package.nix { };
+  slack = pkgs.callPackage ./slack/package.nix { };
+  viber = pkgs.callPackage ./viber/package.nix { };
 in
 {
   imports = [
@@ -43,5 +47,61 @@ in
     ./vscode
   ];
 
-  home.packages = packages;
+  home.packages = with pkgs; [
+    # Essentials
+    bc
+    dig
+    file
+    gcc14
+    gnumake
+    inetutils
+    inotify-tools
+    killall
+    moreutils
+    unzip
+    usbutils
+    wget
+
+    # CLI
+    clipboard2markdown
+    dconf2nix
+    pkgs-unstable.deno
+    ffmpeg
+    (lib.hiPrio file-rename)
+    ghostscript
+    imagemagick
+    libsecret
+    nsx
+    python314
+    python314Packages.uv
+    pkgs-unstable.ocrmypdf
+    qemu
+    rclone
+    scrcpy
+    tesseract
+    trash-cli
+    tree
+    unp
+    wineWowPackages.stable
+    wl-clipboard
+    yt-dlp
+
+    # GUI
+    apostrophe
+    bottles
+    bruno
+    eog
+    gnome-firmware
+    gnome-tweaks
+    google-chrome
+    inkscape
+    keepassxc
+    onlyoffice-desktopeditors
+    papers
+    pkgs-unstable.proton-pass
+    protonvpn-gui
+    remmina
+    slack
+    viber
+  ];
 }
