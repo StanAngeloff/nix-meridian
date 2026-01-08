d78f97ed9ea1bffaab19edee5459215dec9fb175
Stan Angeloff <stanimir@angeloff.name>
2026-01-08 12:27:14 +0200

    feat: add npm registry aliases for Amp Code and Claude Code.

--- home/apps/zsh/aliases.nix
+++ home/apps/zsh/aliases.nix
@@ -1,3 +1,11 @@
+let
+  # List of packages from the npm registry I use often, but can't be bothered to install them system-wide. ( ͡° ͜ʖ ͡°)
+  # Having them aliased to "@latest" also keeps them up-to-date automatically on each use.
+  npm_registry_apps = {
+    amp = "@sourcegraph/amp";
+    claude = "@anthropic-ai/claude-code";
+  };
+in
 {
   programs.zsh.shellAliases = {
     g = "git";
@@ -6,5 +14,6 @@
     t = "tig status";
     v = "nvim -p";
     vim = "nvim -p";
-  };
+  }
+  // builtins.mapAttrs (name: pkg: "pnpm --silent dlx ${pkg}@latest") npm_registry_apps;
 }
